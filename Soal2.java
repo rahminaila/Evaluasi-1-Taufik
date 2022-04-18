2fd5b1e48903e32993dac96c4a74cb68eadb6dff
mohamadtaufikhidayat <mohamadtaufikhidayat65@gmail.com>
2022-04-18 08:24:32 +0700

    saran dari taufik

--- Soal2.java
+++ Soal2.java
@@ -1,13 +1,13 @@
 package bcgen9.day12;
 
 import java.util.Scanner;
-
+//70//34 26 44//21 39 33
 public class Soal2 {
 	Scanner s= new Scanner(System.in);
 	int n;
 	int[] arrKacamata;
 	int[] arrBaju;
-	int[][] gabungan;
+	//int[][] gabungan;
 	int hasil;
 	public int getUangAndi() {
 		System.out.println("Masukkan uang Andi: ");
@@ -30,52 +30,54 @@ public class Soal2 {
 		}
 		return arrBaju;
 	}
-	public void tampungArray() {
-		arrayKacamata();
-		arrayBaju();
-		int [][] gabungan = new int[3][2];
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<2; j++) {
-				if(j==0) {
-					gabungan[i][0]=arrKacamata[i];
-				} else {
-					gabungan[i][1]=arrBaju[i];
-				}
-			
-			System.out.print(gabungan[i][j]);
-			}
-			System.out.println();
-		}
-	}
-	public void sum() {
-		tampungArray();
-		System.out.println();
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<2; j++) {
-				int hasil = gabungan[i][0]+ gabungan[i][0];
-				System.out.println(hasil);
-			}
-		}
-		
-	}
+//	public void tampungArray() {
+//		arrayKacamata();
+//		arrayBaju();
+//		int [][] gabungan = new int[3][2];
+//		for(int i=0; i<3; i++) {
+//			for(int j=0; j<2; j++) {
+//				if(j==0) {
+//					gabungan[i][0]=arrKacamata[i];
+//				} else {
+//					gabungan[i][1]=arrBaju[i];
+//				}
+//			
+//			System.out.print(gabungan[i][j]);
+//			}
+//			System.out.println();
+//		}
+//	}
+//	public void sum() {
+//		tampungArray();
+//		System.out.println();
+//		for(int i=0; i<3; i++) {
+//			for(int j=0; j<2; j++) {
+//				int hasil = gabungan[i][0]+ gabungan[i][0];
+//				System.out.println(hasil);
+//			}
+//		}
+//		
+//	}
 	public void hitung() {
 		arrayKacamata();
 		arrayBaju();
-		int a=0;
+		//int a=0;
 		int nilai=0;
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				this.hasil = arrKacamata[a] +arrBaju[j]; 
-				if (hasil <= n && hasil > nilai) {
-					nilai=hasil;
-				}				
-			}
-			a=a+1;	
-		}
-		if (nilai <= n && nilai > 0) {
+		s.close();										//karena nilai adalah uang yang dipakai andi
+		for(int i=0; i<3; i++) {						//atau nilai adalah uang yang ingin dibelikan 1 kacamata dan 1 baju
+			for(int j=0; j<3; j++) {					//ada celah krn parameter 
+				this.hasil = arrKacamata[i] +arrBaju[j];// if adalah nilai 
+				if (hasil <= n && hasil > nilai) {		//contoh celah jika
+					nilai=hasil;						//uang andi 70 dan semua harga 44
+				}										// maka nilai=0
+			}											// atau untuk  semua harga adalah 0 (gratis)
+			//a=a+1;									//harusnya masih bisa beli barang
+		}												// dengan nilai(uang yang dipakai andi) = 0
+		if (nilai <= n && nilai >= 0) {
 			System.out.println("Uang yang dapat dibelanjakan= " + nilai);
 		} else {
 			System.out.println("Uang anda tidak cukup");
+			System.out.println(nilai);
 		}
 	}
 		
